487610c65c9669d296ead9759d71ae45eb251c0f
vgulch <vgulch@gmail.com>
2020-06-04 00:12:45 +0300

    fix hack personalize

--- hack/ci-mock.sh
+++ hack/ci-mock.sh
@@ -27,7 +27,7 @@ if [ -z "${version}" ]; then
     version="0.4.0"
 else
     image="${repository}:${version}"
-    image_my="${repository}:${version}"
+    image_my="${repository_my}:${version}"
 fi
 
 echo ">>>> Building image ${image} <<<<"

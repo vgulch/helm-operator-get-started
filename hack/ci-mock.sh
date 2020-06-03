8b88c505b4f5a9bb8c373260a594f7579be8880e
vgulch <vgulch@gmail.com>
2020-06-03 14:19:24 +0300

    resources personalized

--- hack/ci-mock.sh
+++ hack/ci-mock.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 repository="stefanprodan/podinfo"
+repository_my="vgulch/podinfo"
 branch="master"
 version=""
 commit=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1 | awk '{print tolower($0)}')
@@ -22,13 +23,16 @@ shift $((OPTIND-1))
 
 if [ -z "${version}" ]; then
     image="${repository}:${branch}-${commit}"
+    image_my="${repository_my}:${branch}-${commit}"
     version="0.4.0"
 else
     image="${repository}:${version}"
+    image_my="${repository}:${version}"
 fi
 
 echo ">>>> Building image ${image} <<<<"
 
 docker build --build-arg GITCOMMIT=${commit} --build-arg VERSION=${version} -t ${image} -f Dockerfile.ci .
 
-docker push ${image}
+docker image tag ${image} ${image_my}
+docker push ${image_my}
